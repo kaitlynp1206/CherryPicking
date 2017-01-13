d33319a0ccdbc363e9c3041fe9916e123efaf1f7
kaitlynp1206 <kaitlyn.p1206@gmail.com>
2017-01-13 12:17:13 -0500

    Update User.java

--- src/User.java
+++ src/User.java
@@ -2,27 +2,47 @@
  * Created by Elizabeth Ip on 2017-01-11.
  */
 import java.net.Socket;
+
 public class User {
     private Socket socket;
-    private String name;
-
-    User(Socket s, String n){
-        socket=s;
-        name=n;
+    private String username;
+    private String groupName;
+    private int awesomePoints = 0;
+    
+    User(Socket socket, String username){
+        this.socket=socket;
+        this.username=username;
     }
 
-    public void setSocket(Socket s){
-        socket=s;
+    public void setSocket(Socket socket){
+        this.socket=socket;
     }
+    
     public Socket getSocket(){
         return socket;
     }
 
-    public void setName(String n){
-        name=n;
+    public void setUsername(String username){
+        this.username=username;
     }
-    public String getName(){
-        return name;
+    
+    public String getUsername(){
+        return username;
     }
-
+    
+    public void setGroupName(String groupName){
+        this.groupName=groupName;
+    }
+    
+    public String getGroupName(){
+        return groupName;
+    }
+    
+    public void addPoints(){
+        awesomePoints++;
+    }
+    
+    public String getScoreDisplay(){
+        return (username + ": " + awesomePoints);
+    }
 }
