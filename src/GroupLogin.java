e644e4f99df9153840a92b1db95dda3055afc596
Kaitlyn <kaitlyn.p1206@gmail.com>
2017-01-17 11:48:11 -0500

    adding gui

---
+++ src/GroupLogin.java
@@ -0,0 +1,150 @@
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+public class GroupLogin {
+
+	private static JFrame groupFrame;
+	private JTextField groupNameLabel;
+	private static String groupName;
+	private static boolean authenticated = false;
+	private static boolean active = true;
+	static boolean gameMaker = false;
+	
+	private static WindowListener windowListener = new WindowAdapter() {
+        @Override
+        public void windowClosing(WindowEvent e) {
+            exit();
+        }
+    };
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void startGroupScreen(){
+		GroupLogin window = new GroupLogin();
+		groupFrame.setVisible(true);
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GroupLogin() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		groupFrame = new JFrame();
+		groupFrame.getContentPane().setBackground(Color.GRAY);
+		groupFrame.getContentPane().setLayout(null);
+
+		JLabel titleLabel = new JLabel("Cherry Picking!");
+		titleLabel.setFont(new Font("Lithos Pro Regular", Font.ITALIC, 31));
+		titleLabel.setBounds(57, 145, 270, 73);
+		groupFrame.getContentPane().add(titleLabel);
+
+		JButton newGameButton = new JButton("NEW GAME");
+		newGameButton.setBounds(69, 270, 118, 23);
+		groupFrame.getContentPane().add(newGameButton);
+
+		JButton joinGameButton = new JButton("JOIN GAME");
+		joinGameButton.setBounds(196, 270, 118, 23);
+		groupFrame.getContentPane().add(joinGameButton);
+
+		ImageIcon cherries = new ImageIcon("cherries.jpg");
+		Image temp = cherries.getImage().getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH);
+		cherries = new ImageIcon(temp);
+		JLabel imageCherry = new JLabel(cherries);
+		imageCherry.setBounds(132, 30, 120, 120);
+		groupFrame.getContentPane().add(imageCherry);
+
+		groupNameLabel = new JTextField(" GAME NAME");
+		groupNameLabel.setBounds(69, 217, 245, 28);
+		groupNameLabel.addMouseListener(new MouseAdapter(){
+			@Override
+			public void mouseClicked(MouseEvent e){
+				groupNameLabel.setText("");
+			}
+		});
+		groupFrame.getContentPane().add(groupNameLabel);
+		
+		JLabel errorLabel = new JLabel("");
+		errorLabel.setBounds(121, 304, 146, 23);
+		groupFrame.getContentPane().add(errorLabel);
+		groupFrame.setBounds(100, 100, 400, 400);
+		groupFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		newGameButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				groupName = groupNameLabel.getText();
+				try {
+					if ((!authenticated) && (Client.authenticateGroupName(groupName))){
+						Client.groupName = groupName;
+						authenticated=true;
+					}
+				} catch (Exception error){
+					error.printStackTrace();
+				}
+				if (authenticated){
+					Client.authenticatedGroup = true;
+					gameMaker=true;
+					close();
+				} else{
+					errorLabel.setText("Error: Group name already exists");
+				}
+			}
+		});
+
+		joinGameButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				groupName = groupNameLabel.getText();
+				try {
+					if ((!authenticated) && (Client.authenticateGroupExists(groupName))){
+						Client.groupName = groupName;
+						authenticated=true;
+					}
+				} catch (Exception error){
+					error.printStackTrace();
+				}
+				if (authenticated){
+					Client.authenticatedGroup = true;
+					close();
+				} else {
+					errorLabel.setText("Error: Group name not found");
+				}
+			}
+		});
+	}
+
+	public static void exit(){
+		groupFrame.dispose();
+		active=false;
+		Client.clientRunning=false;
+	}
+	
+	public void close() {
+		groupFrame.dispose();
+		active=false;
+	}
+	
+	public synchronized boolean getStatus(){
+		return active;
+	}
+}
---
+++ src/UsernameLogin.java
@@ -0,0 +1,134 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Image;
+
+import javax.swing.JTextField;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class UsernameLogin {
+
+	private static JFrame usernameFrame;
+	private JTextField usernameLabel;
+	private JButton startButton;
+	private JLabel imageCherry;
+	private static String username = "";
+	private static boolean authenticated = false;
+	private JLabel errorLabel;
+	private static boolean active = true;
+	
+	private static WindowListener windowListener = new WindowAdapter() {
+        @Override
+        public void windowClosing(WindowEvent e) {
+            exit();
+        }
+    };
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void startUserScreen(){
+		UsernameLogin window = new UsernameLogin();
+		usernameFrame.setVisible(true);
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public UsernameLogin() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		usernameFrame = new JFrame();
+		usernameFrame.getContentPane().setBackground(Color.GRAY);
+		usernameFrame.getContentPane().setLayout(null);
+		
+		JLabel titleLabel = new JLabel("Cherry Picking!");
+		titleLabel.setBounds(57, 145, 270, 73);
+		titleLabel.setFont(new Font("Lithos Pro Regular", Font.ITALIC, 31));
+		usernameFrame.getContentPane().add(titleLabel);
+		
+		usernameLabel = new JTextField("GAME NAME");
+		usernameLabel.setBounds(69, 217, 245, 28);
+		usernameLabel.addMouseListener(new MouseAdapter(){
+			@Override
+			public void mouseClicked(MouseEvent e){
+				usernameLabel.setText("");
+			}
+		});
+		usernameFrame.getContentPane().add(usernameLabel);
+		usernameLabel.setColumns(10);
+		
+		ImageIcon cherries = new ImageIcon("cherries.jpg");
+		Image temp = cherries.getImage().getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH);
+		cherries = new ImageIcon(temp);
+		JLabel imageCherry = new JLabel(cherries);
+		imageCherry.setBounds(132, 30, 120, 120);
+		
+		startButton = new JButton("START!");
+		startButton.setBounds(147, 270, 89, 23);
+		usernameFrame.getContentPane().add(startButton);
+		
+		usernameFrame.getContentPane().add(imageCherry);
+		
+		
+		usernameFrame.setBounds(100, 100, 400, 400);
+		usernameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+		startButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				username = usernameLabel.getText();
+				try {
+					if ((!authenticated) && (Client.authenticateUsername(username))) {
+						Client.username = username;
+						authenticated = true;
+					}
+				} catch (Exception error) {
+					error.printStackTrace();
+				}
+				if (authenticated) {
+					close();
+					Client.authenticatedName = true;
+				} else {
+					// Error label
+					errorLabel = new JLabel("Error: Username has already been taken");
+					errorLabel.setForeground(Color.RED);
+					errorLabel.setBounds(96, 305, 195, 14);
+					usernameFrame.getContentPane().add(errorLabel);
+                }
+			}
+		});
+	
+	}
+	
+	public synchronized boolean getStatus(){
+		return active;
+	}
+	
+	public static void exit(){
+		usernameFrame.dispose();
+		active=false;
+		Client.clientRunning=false;
+	}
+	
+	public void close() {
+		usernameFrame.dispose();
+		active=false;
+		
+	}
+}
