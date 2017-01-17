e9c3b64c9e4ea9da9867c770dd81a7bd61682c7c
kaitlynp1206 <kaitlyn.p1206@gmail.com>
2017-01-17 11:43:17 -0500

    Update GameScreen.java

--- src/GameScreen.java
+++ src/GameScreen.java
@@ -1,12 +1,8 @@
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import java.awt.Font;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.DefaultListModel;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JTextArea;
@@ -16,43 +12,32 @@ import java.awt.Dimension;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowListener;
 import java.awt.event.ActionEvent;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.BorderLayout;
 
 public class GameScreen {
 
 	private Font displayFont = new Font("Bodoni MT", Font.ITALIC, 20);
+	private Font cardFont = new Font("Book Antiqua", Font.PLAIN, 11);
 	private Font groupNameFont = new Font("Bodoni Bd BT", Font.BOLD, 25);
 	private Font countdownFont = new Font("Bodoni MT", Font.PLAIN, 35);
 	public static JFrame frmCherryPicking;
 	static DefaultListModel<String> defaultList = new DefaultListModel<>();
 	private static User currentUser;
-	
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					GameScreen window = new GameScreen();
-					window.frmCherryPicking.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	private static boolean active = true;
+	private String msg;
+	private static int cardSelected=-1;
 
 	private static WindowListener windowListener = new WindowAdapter() {
-        @Override
-        public void windowClosing(WindowEvent e) {
-            close();
-        }
-    };
-	
+		@Override
+		public void windowClosing(WindowEvent e) {
+			exit();
+		}
+	};
+
 	/**
 	 * GameScreen
 	 * Default constructor for GameScreen, initializes GUI
@@ -60,7 +45,7 @@ public class GameScreen {
 	public GameScreen() {
 		initialize();
 	}
-	
+
 	/**
 	 * startGameScreen
 	 * Launch GUI
@@ -70,7 +55,7 @@ public class GameScreen {
 		frmCherryPicking.setVisible(true);
 	}
 
-	
+
 	/**
 	 * getCurrentUser
 	 * @return User - current user on the client 
@@ -78,15 +63,15 @@ public class GameScreen {
 	public static User getCurrentUser(){
 		return currentUser;
 	}
-	
+
 	/**
-	 * 
+	 * updateScoresList
 	 */
 	public static void updateScoresList(){
 		defaultList = new DefaultListModel<>();
-		
+
 	}
-	
+
 	/**
 	 * Initialize the contents of the frame.
 	 */
@@ -94,190 +79,265 @@ public class GameScreen {
 		//set JFrame
 		frmCherryPicking = new JFrame();
 		frmCherryPicking.setTitle("Cherry Picking");
-		frmCherryPicking.setSize(new Dimension(802, 500));
+		frmCherryPicking.setSize(new Dimension(792, 486));
 		frmCherryPicking.setLocationRelativeTo(null);
 		frmCherryPicking.setResizable(false);
 		frmCherryPicking.addWindowListener(windowListener);
 		frmCherryPicking.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmCherryPicking.getContentPane().setLayout(null);
-		
-		
+
+
 		//initialize the current user
 		currentUser = new User();
-		
+
 		//initialize display panel - contains scores and chat room
 		JPanel displayPanel = new JPanel();
 		displayPanel.setBackground(Color.BLACK);
 		displayPanel.setBounds(0, 0, 180, 461);
 		frmCherryPicking.getContentPane().add(displayPanel);
-		
+
 		//groupName display
 		JLabel groupLabel = new JLabel("Group Name:");
+		groupLabel.setBounds(10, 11, 110, 20);
 		groupLabel.setForeground(Color.WHITE);
 		groupLabel.setFont(displayFont);
-		
+
 		JLabel groupNameLabel = new JLabel("New label");
+		groupNameLabel.setBounds(10, 37, 116, 30);
 		groupNameLabel.setForeground(Color.RED);
 		groupNameLabel.setFont(groupNameFont);
-		
+
 		//scoresList display
 		JLabel scoresLabel = new JLabel("Scores:");
+		scoresLabel.setBounds(10, 73, 110, 25);
 		scoresLabel.setForeground(Color.WHITE);
 		scoresLabel.setFont(displayFont);
-		
+
 		JList scoresList = new JList();
-		
+		scoresList.setBounds(10, 104, 160, 128);
+		scoresList.setLayoutOrientation(JList.VERTICAL_WRAP);
+		scoresList.setVisibleRowCount(-1);
+
+		//chat room display
 		JLabel chatLabel = new JLabel("Chat:");
+		chatLabel.setBounds(10, 238, 110, 25);
 		chatLabel.setForeground(Color.WHITE);
 		chatLabel.setFont(displayFont);
-		
+
+		JPanel chatDisplayPanel = new JPanel();
+		chatDisplayPanel.setBounds(10, 264, 160, 131);
+		displayPanel.add(chatDisplayPanel);
+		chatDisplayPanel.setLayout(new BorderLayout(0, 0));
+
 		JTextArea chatDisplay = new JTextArea();
-		
+		chatDisplayPanel.add(new JScrollPane(chatDisplay), BorderLayout.CENTER);
+		chatDisplay.setText("");
+		chatDisplay.setLineWrap(true);
+		chatDisplay.setWrapStyleWord(true);
+		chatDisplay.setBackground(Color.WHITE);
+
+		JPanel chatMessagePanel = new JPanel();
+		chatMessagePanel.setBounds(10, 406, 113, 44);
+		displayPanel.add(chatMessagePanel);
+		chatMessagePanel.setLayout(new BorderLayout(0, 0));
+
 		JTextArea chatMessage = new JTextArea();
-		
+		chatMessagePanel.add(new JScrollPane(chatMessage), BorderLayout.CENTER);
+		chatMessage.setText("");
+		chatMessage.setLineWrap(true);
+		chatMessage.setWrapStyleWord(true);
+		chatMessage.setBackground(Color.WHITE);
+
 		JButton sendButton = new JButton(">");
-		GroupLayout gl_displayPanel = new GroupLayout(displayPanel);
-		gl_displayPanel.setHorizontalGroup(
-			gl_displayPanel.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_displayPanel.createSequentialGroup()
-					.addContainerGap()
-					.addGroup(gl_displayPanel.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_displayPanel.createSequentialGroup()
-							.addComponent(groupLabel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addGap(244))
-						.addGroup(gl_displayPanel.createSequentialGroup()
-							.addComponent(groupNameLabel)
-							.addContainerGap(49, Short.MAX_VALUE))
-						.addGroup(gl_displayPanel.createSequentialGroup()
-							.addComponent(scoresLabel, GroupLayout.PREFERRED_SIZE, 110, GroupLayout.PREFERRED_SIZE)
-							.addContainerGap(55, Short.MAX_VALUE))
-						.addGroup(gl_displayPanel.createSequentialGroup()
-							.addComponent(chatLabel, GroupLayout.PREFERRED_SIZE, 110, GroupLayout.PREFERRED_SIZE)
-							.addContainerGap(55, Short.MAX_VALUE))
-						.addGroup(gl_displayPanel.createSequentialGroup()
-							.addGroup(gl_displayPanel.createParallelGroup(Alignment.TRAILING, false)
-								.addComponent(scoresList, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-								.addComponent(chatDisplay, Alignment.LEADING)
-								.addGroup(Alignment.LEADING, gl_displayPanel.createSequentialGroup()
-									.addComponent(chatMessage, GroupLayout.PREFERRED_SIZE, 113, GroupLayout.PREFERRED_SIZE)
-									.addPreferredGap(ComponentPlacement.RELATED)
-									.addComponent(sendButton)))
-							.addContainerGap(5, Short.MAX_VALUE))))
-		);
-		gl_displayPanel.setVerticalGroup(
-			gl_displayPanel.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_displayPanel.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(groupLabel, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(groupNameLabel)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(scoresLabel, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(scoresList, GroupLayout.PREFERRED_SIZE, 128, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(chatLabel, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
-					.addGap(1)
-					.addComponent(chatDisplay, GroupLayout.PREFERRED_SIZE, 131, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addGroup(gl_displayPanel.createParallelGroup(Alignment.LEADING)
-						.addComponent(chatMessage, GroupLayout.DEFAULT_SIZE, 44, Short.MAX_VALUE)
-						.addComponent(sendButton, GroupLayout.PREFERRED_SIZE, 44, GroupLayout.PREFERRED_SIZE))
-					.addContainerGap())
-		);
-		displayPanel.setLayout(gl_displayPanel);
-		
+		sendButton.setBounds(129, 406, 41, 44);
+
+		displayPanel.setLayout(null);
+		displayPanel.add(groupLabel);
+		displayPanel.add(groupNameLabel);
+		displayPanel.add(scoresLabel);
+		displayPanel.add(chatLabel);
+		displayPanel.add(scoresList);
+		displayPanel.add(sendButton);
+
+		sendButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				msg = chatMessage.getText().trim();
+				if (msg!=null && msg!=""){
+					chatDisplay.append(Client.username + ": " + msg + "\n");
+					chatMessage.setText("");
+				}
+			}
+		});
+
+		//create and display cards
 		JPanel cardsPanel = new JPanel();
 		cardsPanel.setBackground(Color.DARK_GRAY);
 		cardsPanel.setBounds(179, 297, 607, 122);
 		frmCherryPicking.getContentPane().add(cardsPanel);
 		cardsPanel.setLayout(null);
-		
-		JButton card1 = new JButton("New button");
-		card1.setBackground(Color.RED);
-		card1.setBounds(10, 11, 89, 100);
-		cardsPanel.add(card1);
-		
-		JButton card2 = new JButton("New button");
-		card2.setBounds(109, 11, 89, 100);
-		cardsPanel.add(card2);
-		
-		JButton card3 = new JButton("New button");
-		card3.setBounds(208, 11, 89, 100);
-		cardsPanel.add(card3);
-		
-		JButton card4 = new JButton("New button");
-		card4.setBounds(307, 11, 89, 100);
-		cardsPanel.add(card4);
-		
-		JButton card5 = new JButton("New button");
-		card5.setBounds(406, 11, 89, 100);
-		cardsPanel.add(card5);
-		
-		JButton card6 = new JButton("New button");
-		card6.setBounds(505, 11, 89, 100);
-		cardsPanel.add(card6);
-		
+
+		JTextArea handCards[] = new JTextArea[6];
+		for (int i=0; i<6; i++){
+			handCards[i] = new JTextArea();
+			handCards[i].setLineWrap(true);
+			handCards[i].setBounds(10 + 99*i, 11, 89, 100);
+			handCards[i].setEditable(false);
+			handCards[i].setLineWrap(true);
+			handCards[i].setWrapStyleWord(true);
+			cardsPanel.add(handCards[i]);
+
+		}
+		for (JTextArea cards : handCards){
+			cards.addMouseListener(
+					new MouseAdapter(){
+						public void mousePressed(MouseEvent e){
+							for (int i=0; i<6; i++){
+								handCards[i].setBackground(Color.WHITE);
+							}
+							cards.setBackground(Color.RED);
+							System.out.println(cards.getBackground());
+						}
+					});
+		}
+
 		JPanel selectPanel = new JPanel();
 		selectPanel.setBackground(Color.LIGHT_GRAY);
 		selectPanel.setBounds(179, 418, 607, 43);
 		frmCherryPicking.getContentPane().add(selectPanel);
-		
+
 		JButton selectCardButton = new JButton("Select Card");
 		selectCardButton.setBounds(205, 11, 196, 21);
 		selectCardButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
+			public void actionPerformed(ActionEvent e) {
+				for (int i=0; i<6; i++){
+					//check for selected card
+					if (handCards[i].getBackground()==Color.RED){
+						cardSelected=i;
+					}else{//card not selected
+						cardSelected=-1;
+					}
+				}
+				
+				if (cardSelected!=-1){
+					
+				}
 			}
 		});
 		selectPanel.setLayout(null);
 		selectPanel.add(selectCardButton);
-		
+
 		JPanel personalPanel = new JPanel();
 		personalPanel.setBackground(Color.LIGHT_GRAY);
 		personalPanel.setBounds(179, 255, 607, 43);
 		frmCherryPicking.getContentPane().add(personalPanel);
 		personalPanel.setLayout(null);
-		
+
 		JButton exitButton = new JButton("Leave Game");
 		exitButton.setBounds(491, 11, 106, 23);
+		exitButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				exit();
+			}
+		});
 		personalPanel.add(exitButton);
-		
+
 		JLabel usernameLabel = new JLabel("Username");
 		usernameLabel.setForeground(Color.BLACK);
 		usernameLabel.setFont(new Font("Bodoni MT", Font.ITALIC, 26));
-		usernameLabel.setBounds(10, 11, 399, 20);
+		usernameLabel.setBounds(10, 11, 185, 20);
 		personalPanel.add(usernameLabel);
-		
+
+		if (GroupLogin.gameMaker=true){
+			JButton startGameButton = new JButton("Start Game");
+			startGameButton.setBounds(375, 11, 106, 23);
+			startGameButton.addActionListener(new ActionListener(){
+				public void actionPerformed(ActionEvent e){
+					//check number of players
+				}
+			});
+			personalPanel.add(startGameButton);
+		}
+
 		JPanel gamePanel = new JPanel();
 		gamePanel.setBackground(Color.DARK_GRAY);
 		gamePanel.setBounds(179, 0, 607, 256);
 		frmCherryPicking.getContentPane().add(gamePanel);
 		gamePanel.setLayout(null);
-		
-		JPanel blackPanel = new JPanel();
+
+		JTextArea blackPanel = new JTextArea();
+		blackPanel.setLineWrap(true);
+		blackPanel.setText("Waiting for more players...");
+		blackPanel.setForeground(Color.RED);
 		blackPanel.setBackground(Color.BLACK);
 		blackPanel.setBounds(10, 11, 143, 147);
 		gamePanel.add(blackPanel);
-		
+
 		JPanel resultsPanel = new JPanel();
 		resultsPanel.setBackground(Color.BLACK);
 		resultsPanel.setBounds(163, 11, 434, 234);
 		gamePanel.add(resultsPanel);
-		
+		resultsPanel.setLayout(null);
+
+		JTextArea card5 = new JTextArea();
+		card5.setBackground(Color.LIGHT_GRAY);
+		card5.setBounds(10, 126, 93, 97);
+		resultsPanel.add(card5);
+
+		JTextArea card1 = new JTextArea();
+		card1.setBackground(Color.LIGHT_GRAY);
+		card1.setBounds(10, 11, 93, 97);
+		resultsPanel.add(card1);
+
+		JTextArea card6 = new JTextArea();
+		card6.setBackground(Color.LIGHT_GRAY);
+		card6.setBounds(117, 126, 93, 97);
+		resultsPanel.add(card6);
+
+		JTextArea card2 = new JTextArea();
+		card2.setBackground(Color.LIGHT_GRAY);
+		card2.setBounds(117, 11, 93, 97);
+		resultsPanel.add(card2);
+
+		JTextArea card7 = new JTextArea();
+		card7.setBackground(Color.LIGHT_GRAY);
+		card7.setBounds(224, 126, 93, 97);
+		resultsPanel.add(card7);
+
+		JTextArea card3 = new JTextArea();
+		card3.setBackground(Color.LIGHT_GRAY);
+		card3.setBounds(224, 11, 93, 97);
+		resultsPanel.add(card3);
+
+		JTextArea card8 = new JTextArea();
+		card8.setBackground(Color.LIGHT_GRAY);
+		card8.setBounds(331, 126, 93, 97);
+		resultsPanel.add(card8);
+
+		JTextArea card4 = new JTextArea();
+		card4.setBackground(Color.LIGHT_GRAY);
+		card4.setBounds(331, 11, 93, 97);
+		resultsPanel.add(card4);
+
 		JLabel countdownLabel = new JLabel("Countdown:");
 		countdownLabel.setForeground(Color.WHITE);
 		countdownLabel.setFont(new Font("Bodoni MT", Font.ITALIC, 20));
 		countdownLabel.setBounds(10, 169, 110, 20);
 		gamePanel.add(countdownLabel);
-		
+
 		JLabel timerLabel = new JLabel("00:00:00");
 		timerLabel.setForeground(Color.WHITE);
 		timerLabel.setFont(new Font("Bodoni MT", Font.PLAIN, 35));
 		timerLabel.setBounds(20, 200, 120, 33);
 		gamePanel.add(timerLabel);
 	}
-	
-	public static void close(){
+
+	public static void exit(){
 		frmCherryPicking.dispose();
+		active=false;
+		Client.clientRunning=false;
+	}
+
+	public synchronized boolean getStatus(){
+		return active;
 	}
 }
