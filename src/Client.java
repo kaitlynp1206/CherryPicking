b2bdbf04e3cd803ddfb808c6d791abe78cb905f1
kaitlynp1206 <kaitlyn.p1206@gmail.com>
2017-01-17 11:44:30 -0500

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,8 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 /**
@@ -9,11 +13,31 @@ public class Client {
     private static PrintWriter writer;
     private static BufferedReader reader;
     private static BufferedReader consoleReader;
-
+    static String username;
+    static String groupName;
+    static boolean authenticatedName = false;
+    static boolean authenticatedGroup = false;
+    static volatile boolean clientRunning;
+    
+    
     public static void main (String args[]){
-        new Client().go();
+        clientRunning = true;
+    	Client client = new Client();
+    	client.go();
     }
 
+    public static boolean authenticateUsername(String username) throws Exception{
+    	return true;
+    }
+    
+    public static boolean authenticateGroupExists(String groupName) throws Exception{
+    	return true;
+    }
+    
+    public static boolean authenticateGroupName(String groupName) throws Exception{
+    	return true;
+    }
+    
     public void go(){
         String text="";
 
@@ -31,6 +55,24 @@ public class Client {
 
         }catch(IOException e){
             System.out.println(e);
+        }
+        
+        //Username Screen
+        UsernameLogin userLogin = new UsernameLogin();
+        userLogin.startUserScreen();
+        while (userLogin.getStatus()){
+        }
+        
+      //GroupName Screen
+        GroupLogin groupLogin = new GroupLogin();
+        groupLogin.startGroupScreen();
+        while (groupLogin.getStatus()){
+        }
+        
+        //GameScreen
+        GameScreen gameDisplay = new GameScreen();
+        gameDisplay.startGameScreen();
+        while (gameDisplay.getStatus()){
         }
     }
 
