a2afa006953e2c84246a60fe48e740b1c6bdf81a
kaitlynp1206 <kaitlyn.p1206@gmail.com>
2017-01-23 01:34:48 -0500

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -8,7 +8,11 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
- * Created by Elizabeth Ip on 2017-01-08.
+ * Client.java
+ * @author Kaitlyn Paglia & Elizabeth Ip
+ * @version 1.0
+ * 1/23/2017
+ * Client communicating with Server and storing Client's information to be used by GUI
  */
 public class Client {
 	private static Socket socket;
@@ -24,19 +28,32 @@ public class Client {
 	private static boolean authenticatedName = false;
 	private static boolean authenticatedGroup = false;
 	private static int gameSelection=0;
-	private static boolean creator;//tells if person created game or joined it
-	static ArrayList<Card>hand;//COPY THIS
-	static ArrayList<Card> selected;//COPY THIS TOO
+	private static boolean creator;
+	static ArrayList<Card>hand;
+	static ArrayList<Card> selected;
 	private static boolean proceed = false;
 	private static int selectedCard;
 	private static boolean validCard;
 
+	static ArrayList<String>players;
+	static ArrayList<Integer>scores;
 
+	private static int winningCard;
+	private static String chatMessage = "";
 
+	/**
+	 * main
+	 * Initializes Client
+	 * @param args
+	 */
 	public static void main (String args[]){
 		new Client().go();
 	}
 
+	/**
+	 * go
+	 * Connects to server socket
+	 */
 	public void go(){
 		try {
 			//declare stuff so no null pointer exceptions
@@ -109,7 +126,10 @@ public class Client {
 			}
 		}
 
-		//runs the client side of every game
+		/**
+		 * run
+		 * Executes the Client's side of the communication
+		 */
 		public synchronized void run(){
 			try {
 				while(!playing){//while the user is not in any game, prompt to join or create one
@@ -146,25 +166,26 @@ public class Client {
 								System.out.println("Check");
 								creator=true;
 								System.out.println("y"+start);
-								while(!start){
+								/*while(!start){
 									System.out.println(start+" GEL");
 								}
 								System.out.println("x"+start);
 								writer.println("/start game/");//tells server to start the game, u can replace this with a start button and only have it show if the player created the game
 								playing=true;//allow user to move on
-								System.out.println(playing);
+								System.out.println(playing);*/
 							}
+							playing=true;
 						}
 
 					}
 				}
 
-				while(playing) {//game loop basically
-					System.out.println("IM HERE");
+				while (playing) {//game loop basically
 					if (reader.ready()) {//if server sent a message
 						msg = reader.readLine();
 						validCard = false;
-						System.out.println("C "+msg);
+						System.out.println("C: "+msg);
+						//Receiving and Display hand
 						if (msg.contains("/your hand/")) {//if receiving hand of cards, print them all out
 							msg = msg.substring(msg.lastIndexOf("/") + 1);//remove the "/your hand/" part of the message
 
@@ -173,11 +194,16 @@ public class Client {
 								hand.add(new Card(msg.substring(0, msg.indexOf("(")), Integer.valueOf(msg.substring(msg.indexOf("(") + 1, msg.indexOf(")")))));
 								msg = msg.substring(msg.indexOf("+") + 1);//remove first card from string
 							}
-							System.out.println(hand.size());
-							GameScreen.refreshCards();
+							System.out.println("YOUR HAND: ");//COPY THIS just for aesthetics
+							for (Card c : hand) {//display cards with card numbers
+								System.out.println("CARD " + c.getID() + ": " + c.getText());
+							}
+
+							//writer.println("/ready/" + username);//tell server to go to next stage
 						}
-						else if(msg.contains("/selected cards/")){//display the selected cards
-							msg=msg.substring(msg.lastIndexOf("/")+1);
+						//Displaying everyone's cards
+						else if (msg.contains("/selected cards/")) {//display the selected cards
+							msg = msg.substring(msg.lastIndexOf("/") + 1);
 							selected.clear();//empty the current hand, replace with new hand
 							while (msg.length() > 1) {//display cards
 								selected.add(new Card(msg.substring(0, msg.indexOf("(")), Integer.valueOf(msg.substring(msg.indexOf("(") + 1, msg.indexOf(")")))));
@@ -187,65 +213,89 @@ public class Client {
 							for (Card c : selected) {//display cards with card numbers
 								System.out.println("CARD " + c.getID() + ": " + c.getText());
 							}
-						}
-						else if (msg.contains("/pick card/")) {//if player is prompted to pick a card
+							//Client inputs their selected Card
+						} else if (msg.contains("/pick card/")) {//if player is prompted to pick a card
+							System.out.print("select the number of the card you want: ");
+							while (!start){
+							}
 							for (Card c : hand) {//see if player's selected card matches any of the ones in their hand
-								if (Integer.valueOf(selectedCard) == c.getID()) {
+								if (Integer.valueOf(getSelectedCard()) == c.getID()) {
 									validCard = true;
 								}
 							}
 							while (!validCard) {//error checking
 								System.out.println("invalid number. try again: ");
+								while (!start){
+								}
 								for (Card c : hand) {//see if player's selected card matches any of the ones in their hand
-									if (Integer.valueOf(selectedCard) == c.getID()) {
+									if (Integer.valueOf(getSelectedCard()) == c.getID()) {
 										validCard = true;
 									}
 								}
 							}
-							writer.println("/ready/" + username + "/card/" + selectedCard);//tell the server which card the player picked
+							writer.println("/ready/" + username + "/card/" + getSelectedCard());//tell the server which card the player picked
 
 						}
-						/**COPY SINGLE ELSE IF**/
-						else if(msg.contains("/pick winner/")){
+						else if (msg.contains("/pick winner/")) {
+							System.out.print("select the number of the card you think should win: ");
+
 							for (Card c : selected) {//see if player's selected card matches any of the ones in the selected array
-								if (Integer.valueOf(selectedCard) == c.getID()) {
+								if (Integer.valueOf(getWinningCard()) == c.getID()) {
 									validCard = true;
 								}
 							}
 							while (!validCard) {//error checking
 								System.out.println("invalid number. try again: ");
 								for (Card c : selected) {//see if player's selected card matches any of the ones in their hand
-									if (Integer.valueOf(selectedCard) == c.getID()) {
+									if (Integer.valueOf(getWinningCard()) == c.getID()) {
 										validCard = true;
 									}
 								}
 							}
-							writer.println("/ready/" + username + "/card/" + selectedCard);//tell the server which card the player picked
+							writer.println("/ready/" + username + "/card/" + getWinningCard());//tell the server which card the player picked
 
-						} else if (msg.contains("/you are czar/")){ //send ready message right away if player is czar -- they don't need to pick an adjective card
+						} else if (msg.contains("/you are czar/")) { //send ready message right away if player is czar -- they don't need to pick an adjective card
 							System.out.println("you are the czar. waiting for players to send in selections");
-							writer.println("/ready/"+username);
+							writer.println("/ready/" + username);
 						}
-						/**COPY ELSE IFS**/
-						else if(msg.contains("/winner/")){//if the winner has been picked and server is waiting to start next round
-							if(msg.contains("/card/")) {
+						else if (msg.contains("/winner/")) {//if the winner has been picked and server is waiting to start next round
+							if (msg.contains("/card/")) {
 								System.out.println("WINNER: " + msg.substring(8, msg.indexOf("/card/")));
 								System.out.println("CARD: " + msg.substring(msg.indexOf("/card/") + 1));
 								System.out.print("enter anything to start the next round: ");
 								//text = input.nextLine();
 								writer.println("/ready/" + username);
-							}else{//if the game is over
+							} else {//if the game is over
 
 							}
-						}else if(msg.contains("/confirm start/")){
-							writer.println("/ready/"+username+"/card/");
-						} else if(msg.contains("/game over/")){
-							System.out.println("GAME OVER! WINNER: "+msg.substring(11));//tell player game is over
+						} else if (msg.contains("/confirm start/")) {
+							writer.println("/ready/" + username + "/card/");
+						} else if (msg.contains("/game over/")) {
+							System.out.println("GAME OVER! WINNER: " + msg.substring(11));//tell player game is over
 							System.out.print("enter anything to exit the game: ");
-							//text=input.nextLine();
-							writer.println("/exit/"+username);//tell server user has disconnected
-							playing=false;
-						} else{
+							//text = input.nextLine();
+							writer.println("/exit/" + username);//tell server user has disconnected
+							playing = false;
+						}
+						else if(msg.contains("/chat/")){
+							setChatMessage(msg.substring(7));
+						}
+						//COPY thiS ELSE IF
+						else if(msg.contains("/scores/")){
+							msg=msg.substring(8);
+							players.clear();
+							scores.clear();
+							while(msg.length()>1){
+								players.add(msg.substring(0,msg.indexOf("(")));
+								scores.add(Integer.valueOf(msg.substring(msg.indexOf("(")+1,msg.indexOf(")"))));
+								msg=msg.substring(msg.indexOf("+")+1);
+							}
+							//print the scores and players out
+							for(int i=0;i<players.size();i++){
+								System.out.println(players.get(i)+": "+scores.get(i));
+							}
+						}
+						else{
 							System.out.println(msg);
 						}
 					}
@@ -256,12 +306,19 @@ public class Client {
 		}
 	}
 
+	//Getters and Setters to be used by inherited classes
+	
 	public void startGame(){
 		writer.println("/start game/");
 	}
 
 	public void startRun(){
 		start=true;
+		System.out.println(start);
+	}
+
+	public void stop(){
+		start=false;
 	}
 
 	public void stopRun(){
@@ -332,9 +389,25 @@ public class Client {
 	public int getSelectedCard(){
 		return selectedCard;
 	}
-	
+
 	public void setMsg(String message){
 		msg=message;
+	}
+
+	public int getWinningCard(){
+		return winningCard;
+	}
+
+	public void setWinningCard(int x){
+		winningCard=x;
+	}
+
+	public synchronized String getChatMessage(){
+		return chatMessage;
+	}
+
+	public void setChatMessage(String m){
+		chatMessage=m;
 	}
 
 }
