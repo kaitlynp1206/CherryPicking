4f1fbf40c529f3e953bc9d4936d5c806e0a6bc7b
kaitlynp1206 <kaitlyn.p1206@gmail.com>
2017-01-23 01:35:20 -0500

    Update HelpScreen.java

--- src/HelpScreen.java
+++ src/HelpScreen.java
@@ -2,24 +2,51 @@ import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.Image;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
 
+/**
+ * HelpScreen.java
+ * @author Kaitlyn Paglia
+ * @version 1.0
+ * 1/23/2017
+ * Displays instructions/help
+ */
 public class HelpScreen {
 
-	private JFrame helpFrame;
+	private static JFrame helpFrame;
 
+	/**
+	 * startHelpScreen
+	 * Creates window/frame
+	 */
 	public static void startHelpScreen(){
-		HelpScreen window = new HelpScreen();
-		window.helpFrame.setVisible(true);
+		helpFrame.setVisible(true);
 	}
 
+	/**
+	 * HelpScreen constructor
+	 */
 	public HelpScreen() {
 		initialize();
 	}
+	
+	/**
+	 * Window Listener
+	 * set only current frame visibility to false when closing
+	 */
+	private static WindowListener windowListener = new WindowAdapter() {
+		@Override
+		public void windowClosing(WindowEvent e) {
+			helpFrame.setVisible(false);
+		}
+	};
 
 	/**
 	 * Initialize the contents of the frame.
@@ -29,22 +56,24 @@ public class HelpScreen {
 		helpFrame.getContentPane().setBackground(Color.GRAY);
 		helpFrame.getContentPane().setLayout(null);
 
+		//creates images of cherries display
 		ImageIcon cherries = new ImageIcon("cherries.jpg");
 		Image temp = cherries.getImage().getScaledInstance(50, 50, java.awt.Image.SCALE_SMOOTH);
 		cherries = new ImageIcon(temp);
 		JLabel imageCherry = new JLabel(cherries);
 		imageCherry.setBounds(10, 11, 73, 73);
 		helpFrame.getContentPane().add(imageCherry);
-
 		JLabel imageCherry2 = new JLabel(cherries);
 		imageCherry2.setBounds(351, 11, 73, 73);
 		helpFrame.getContentPane().add(imageCherry2);
 
+		//screen title label
 		JLabel label = new JLabel("Cherry Picking!");
 		label.setFont(new Font("Lithos Pro Regular", Font.ITALIC, 31));
 		label.setBounds(82, 11, 270, 73);
 		helpFrame.getContentPane().add(label);
 
+		//add instructions
 		JTextArea txtrInstructions = new JTextArea();
 		txtrInstructions.setLineWrap(true);
 		txtrInstructions.setWrapStyleWord(true);
@@ -55,6 +84,7 @@ public class HelpScreen {
 		helpFrame.getContentPane().add(txtrInstructions);
 
 		helpFrame.setBounds(100, 100, 450, 300);
+		helpFrame.setResizable(false);
 		helpFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
